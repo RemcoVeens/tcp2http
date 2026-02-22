f4fd0b4b2fe945ee9abd2b1ab6b53ef36f31f7d1
RemcoVeens <remco@track32.nl>
2026-02-22 15:28:46 +0100

    uses internal package

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -2,39 +2,10 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net"
-	"strings"
-)
 
-func getLinesChannel(f net.Conn) <-chan string {
-	ch := make(chan string)
-	// defer close(ch)
-	go func() {
-		var line string
-		defer close(ch)
-		for {
-			buffer := make([]byte, 8)
-			n, err := f.Read(buffer)
-			if err != nil {
-				if err == io.EOF {
-					break
-				}
-				fmt.Println("Error reading file:", err)
-				break
-			}
-			parts := strings.Split(string(buffer[:n]), "\n")
-			line += parts[0]
-			if len(parts) > 1 {
-				ch <- fmt.Sprintf("%s\n", line)
-				line = parts[1]
-			}
-		}
-		ch <- line
-		fmt.Println("listener closed")
-	}()
-	return ch
-}
+	"github.com/RemcoVeens/tcp2http/internal/request"
+)
 
 func main() {
 	listener, err := net.Listen("tcp", "127.0.0.1:42069")
@@ -50,8 +21,12 @@ func main() {
 	}
 	defer conn.Close()
 	fmt.Println("listener accepted")
-	ch := getLinesChannel(conn)
-	for line := range ch {
-		fmt.Printf("read: %s", line)
+	req, err := request.RequestFromReader(conn)
+	if err != nil {
+		fmt.Println("Error reading request:", err)
+		return
 	}
+	fmt.Printf(
+		"Request line:\n- Method: %s\n- Target: %s\n- Version: %s\n",
+		req.RequestLine.Method, req.RequestLine.RequestTarget, req.RequestLine.HttpVersion)
 }
