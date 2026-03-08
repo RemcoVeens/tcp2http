7fad8bf547147e7b9d52e4be1eee9ada16a2769b
RemcoVeens <remco@track32.nl>
2026-03-08 15:52:48 +0100

    more headers

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -18,12 +18,22 @@ func (h Headers) Parse(data []byte) (n int, done bool, err error) {
 	line := string(data[:idx])
 	parts := strings.Split(line, ":")
 	if len(parts) >= 2 {
-		head := parts[0]
+		head := strings.ToLower(parts[0])
 		if strings.Contains(head, " ") {
 			return n, false, fmt.Errorf("invalid header: %s", head)
 		}
+		for _, c := range head {
+			if (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || strings.ContainsRune("!#$%&'*+-.^_`|~", c) {
+				continue
+			}
+			return n, false, fmt.Errorf("invalid header: %s", head)
+		}
 		value := strings.TrimSpace(strings.Join(parts[1:], ":"))
-		h[head] = value
+		if _, ok := h[head]; ok {
+			h[head] += ", " + value
+		} else {
+			h[head] = value
+		}
 	}
 	n += idx + 2
 	data = data[idx+2:]
--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -14,7 +14,7 @@ func TestHeaders(t *testing.T) {
 	n, done, err := headers.Parse(data)
 	require.NoError(t, err)
 	require.NotNil(t, headers)
-	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, "localhost:42069", headers["host"])
 	assert.Equal(t, 23, n)
 	assert.False(t, done)
 
@@ -32,7 +32,7 @@ func TestHeadersEdgeCases(t *testing.T) {
 		data := []byte("Host: localhost:42069\r\n")
 		n, done, err := headers.Parse(data)
 		require.NoError(t, err)
-		assert.Equal(t, "localhost:42069", headers["Host"])
+		assert.Equal(t, "localhost:42069", headers["host"])
 		assert.Equal(t, len(data), n)
 		assert.False(t, done)
 	})
@@ -42,14 +42,14 @@ func TestHeadersEdgeCases(t *testing.T) {
 		data := []byte("Host:    localhost:42069   \r\n")
 		n, done, err := headers.Parse(data)
 		require.NoError(t, err)
-		assert.Equal(t, "localhost:42069", headers["Host"])
+		assert.Equal(t, "localhost:42069", headers["host"])
 		assert.Equal(t, len(data), n)
 		assert.False(t, done)
 	})
 
 	t.Run("Valid 2 headers with existing headers", func(t *testing.T) {
 		headers := NewHeaders()
-		headers["Existing"] = "keep"
+		headers["existing"] = "keep"
 
 		header1 := []byte("Host: localhost:42069\r\n")
 		header2 := []byte("User-Agent: test\r\n")
@@ -65,9 +65,9 @@ func TestHeadersEdgeCases(t *testing.T) {
 		assert.Equal(t, len(header2), n)
 		assert.False(t, done)
 
-		assert.Equal(t, "keep", headers["Existing"])
-		assert.Equal(t, "localhost:42069", headers["Host"])
-		assert.Equal(t, "test", headers["User-Agent"])
+		assert.Equal(t, "keep", headers["existing"])
+		assert.Equal(t, "localhost:42069", headers["host"])
+		assert.Equal(t, "test", headers["user-agent"])
 	})
 
 	t.Run("Valid done", func(t *testing.T) {
@@ -83,7 +83,7 @@ func TestHeadersEdgeCases(t *testing.T) {
 		require.NoError(t, err)
 		assert.Equal(t, 2, n)
 		assert.True(t, done)
-		assert.Equal(t, "localhost:42069", headers["Host"])
+		assert.Equal(t, "localhost:42069", headers["host"])
 	})
 
 	t.Run("Invalid spacing header", func(t *testing.T) {
@@ -94,4 +94,41 @@ func TestHeadersEdgeCases(t *testing.T) {
 		assert.Equal(t, 0, n)
 		assert.False(t, done)
 	})
+}
+
+func TestSpecialLetters(t *testing.T) {
+	headers := NewHeaders()
+	data := []byte("H@st: Localhost:42069\r\n")
+	n, done, err := headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+}
+
+func TestMultieLineSameKey(t *testing.T) {
+	headers := NewHeaders()
+	header1 := []byte("Set-Person: lane-loves-go\r\n")
+	header2 := []byte("Set-Person: prime-loves-zig\r\n")
+	header3 := []byte("Set-Person: tj-loves-ocaml\r\n")
+	data := append(header1, header2...)
+	data = append(data, header3...)
+
+	n, done, _ := headers.Parse(header1)
+	expected := "lane-loves-go"
+	assert.Contains(t, headers, "set-person")
+	assert.Equal(t, expected, headers["set-person"])
+	assert.False(t, done)
+
+	n, done, _ = headers.Parse(header2)
+	expected = "lane-loves-go, prime-loves-zig"
+	assert.Contains(t, headers, "set-person")
+	assert.Equal(t, expected, headers["set-person"])
+	assert.False(t, done)
+
+	n, done, _ = headers.Parse(header3)
+	expected = "lane-loves-go, prime-loves-zig, tj-loves-ocaml"
+	assert.Contains(t, headers, "set-person")
+	assert.Equal(t, expected, headers["set-person"])
+	assert.Equal(t, len(header3), n)
+	assert.False(t, done)
 }
